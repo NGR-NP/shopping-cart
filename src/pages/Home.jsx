b1e6c6eb710824b124cdb604e2a1bbb25c0d16f5
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-03-18 17:20:27 +0000

    commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../sections/Nav"
+import ProductSec from "../sections/ProductsSec"
+
+const Home = () => {
+    return (
+        <>
+            <NavBar />
+            <ProductSec />
+        </>
+    )
+}
+export default Home
---
+++ src/sections/ProductsSec.jsx
@@ -0,0 +1,16 @@
+import ProductCard from "../components/Home/ProductCard"
+
+const ProductSec = () => {
+    return (
+
+<div className="flex gap-3 flex-wrap item-center justify-center p-6">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+
+
+        </div>
+    )
+}
+export default ProductSec
